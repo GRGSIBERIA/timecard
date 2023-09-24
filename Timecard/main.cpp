c00080466bbd3b894b181a20e3ca717436192ff9
Eiichi Takebuchi <nanashi4129@gmail.com>
2023-09-24 16:49:21 +0900

    基本機能を実装

--- Timecard/main.cpp
+++ Timecard/main.cpp
@@ -1,5 +1,12 @@
 #include "myws.h"
 
 int main() {
-	
+	const int BACKLOG = 5;
+	const int READ_TIMEOUT = 10;
+	const size_t ERR_SIZE = 512;
+	char error[ERR_SIZE];
+	int sockfd_listen;
+	int sockfd_client;
+
+
 }
